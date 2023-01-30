b573001f5a8fc90ea3e1f0278519a22e83963ed4
BoasK4 <karen_vbf@hotmail.com>
2023-01-29 23:09:05 -0300

    Aula de validação em function, e utilização de array e for loop

--- web/Origamid.js/script.js
+++ web/Origamid.js/script.js
@@ -47,4 +47,30 @@
 //     nomeCompleto = "Karen Vilas Boas", 
 //     birthday = "04-03-2022", 
 //     lenght = 1.65, 
-//     weight = 58)
+//     weight = 58;
+
+// let nome = ["K", "A", 0, "E", false];
+
+// for(let i = 0; i < nome.length; i++){
+//     if(typeof nome[i] === "string") {
+//         console.log(nome[i]);
+//     }
+// }
+
+// function imc(nome, altura, peso){
+//     if(typeof altura === "number" && typeof peso === "number") {
+//         let imc = peso / (altura * altura) 
+       
+//         if (imc < 18.5){
+//             console.log(nome + " seu IMC deu " + imc + " = Baixo peso");
+//         } else if (imc >= 18.5 && imc < 24.99){
+//             console.log(nome + " seu IMC deu " + imc + " = Normal");
+//         } else {
+//             console.log(nome + " seu IMC é " + imc + " = Está com sobrepeso");
+//         }
+//     } else {
+//         console.log("Tipo de valor é inválido");
+//     }
+// }
+
+// imc("Karen ", 1.65, 58);
