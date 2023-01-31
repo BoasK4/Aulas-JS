3e9edf4ec760d914386c1d0a61bfdf33921462e6
BoasK4 <karen_vbf@hotmail.com>
2023-01-30 23:31:47 -0300

    Realocação de pastas

--- AulasMat/compilado.js
+++ AulasMat/compilado.js
@@ -119,8 +119,33 @@
 //     console.log ("Pesquise a previsão do tempo");
 // }
 
+// Aula - 29/01/2023
 
+// let nome = ["K", "A", 0, "E", false];
 
+// for(let i = 0; i < nome.length; i++){
+//     if(typeof nome[i] === "string") {
+//         console.log(nome[i]);
+//     }
+// }
+
+// function imc(nome, altura, peso){
+//     if(typeof altura === "number" && typeof peso === "number") {
+//         let imc = peso / (altura * altura) 
+       
+//         if (imc < 18.5){
+//             console.log(nome + " seu IMC deu " + imc + " = Baixo peso");
+//         } else if (imc >= 18.5 && imc < 24.99){
+//             console.log(nome + " seu IMC deu " + imc + " = Normal");
+//         } else {
+//             console.log(nome + " seu IMC é " + imc + " = Está com sobrepeso");
+//         }
+//     } else {
+//         console.log("Tipo de valor é inválido");
+//     }
+// }
+
+// imc("Karen ", 1.65, 58);
 
 
 
